37973d868df2667dd7dcb6a797b8342682c9a879
harshitsingh45 <harshitsingh45@gmail.com>
2022-02-04 11:38:41 +0530

    Function problems

---
+++ FUNCTIONS/Addition.java
@@ -0,0 +1,15 @@
+package FUNCTIONS;
+
+public class Addition {
+	public static int Add(int n1,int n2) {
+		int result=n1+n2;
+		return result;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(Add(10,6));
+
+	}
+
+}
